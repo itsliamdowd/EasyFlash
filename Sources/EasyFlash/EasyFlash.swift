7fa9b86748f40afbc08bf00b34bb98a4b94da931
SkiingIsFun123 <101684827+SkiingIsFun123@users.noreply.github.com>
2022-07-01 01:33:35 -0700

    Release 1.0

--- Sources/EasyFlash/EasyFlash.swift
+++ Sources/EasyFlash/EasyFlash.swift
@@ -1,6 +1,31 @@
-public struct EasyFlash {
-    public private(set) var text = "Hello, World!"
+import UIKit
+import Foundation
+import AVFoundation
 
-    public init() {
+public struct EasyFlash {
+    public static func flashOff() {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+
+        do {
+            try device.lockForConfiguration()
+            device.torchMode = AVCaptureDevice.TorchMode.off
+
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
+    
+    public static func flashOn() {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+
+        do {
+            try device.lockForConfiguration()
+            try device.setTorchModeOn(level: 1.0)
+        } catch {
+            print(error)
+        }
     }
 }
